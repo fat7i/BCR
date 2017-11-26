42e2f5b8e29723287282be8f2370cd15dc677fbe
Fat7i <fat7i.wp@gmail.com>
2017-11-26 22:20:23 +0300

    fix

--- resources/views/layouts/t1.blade.php
+++ resources/views/layouts/t1.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="UTF-8">
-    <title>@if(@$page_title){{ $page_title }}@else{{ config('app.name', 'Laravel') }}</title>
+    <title>@if(@$page_title){{ $page_title }}@else{{ config('app.name', 'Laravel') }}@endif</title>
     <meta name="viewport" content="width=device-width, initial-scale=1  maximum-scale=1 user-scalable=no">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="HandheldFriendly" content="True">
